2bdafb964ba4793d9d74dbcbf0063cf993e85104
Isaiah John G. Garciano <151437600+ijggarz@users.noreply.github.com>
2024-11-01 18:44:11 +0800

    test

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -454,7 +454,6 @@ const AccountManagement = () => {
   return (
   <div style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}> 
     {/* Always show the AppBar */}
-    <LoginResponsiveAppBar />
 
     {/* Background Logo */}
     <img
