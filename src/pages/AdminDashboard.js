cdf619e65d7ed33e42a0fcdce72b5f4f6cc11d68
Isaiah John G. Garciano <151437600+ijggarz@users.noreply.github.com>
2024-11-10 20:12:17 +0800

    remove bg logo(admin)

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -451,22 +451,6 @@ const AccountManagement = () => {
     {/* Always show the AppBar */}
     <LoginResponsiveAppBar />
 
-    {/* Background Logo */}
-    <img
-      src="/logo.png"
-      alt="Background Logo"
-      style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)', 
-        opacity: 0.3, 
-        width: '1000px',
-        height: 'auto',
-        zIndex: -1,
-      }}
-    />
-
     {isLoading ? (
       <Box
         sx={{
