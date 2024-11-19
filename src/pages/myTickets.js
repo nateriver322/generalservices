4f704f6ea930104308353f241555e217ba6cf7c1
ethannjan <166780996+ethannjan@users.noreply.github.com>
2024-11-19 19:29:03 +0800

    tryu

--- src/pages/myTickets.js
+++ src/pages/myTickets.js
@@ -142,11 +142,11 @@ const MyTickets = () => {
   const [feedbackSuccessSnackbarOpen, setFeedbackSuccessSnackbarOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [tabValue, setTabValue] = useState(0);
-
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
-    navigate(newValue === 0 ? '/tickets' : '/archived');
+    navigate(newValue === 0 ? '/tickets' : '/archived'); // Changed the URL for the Archived tab
   };
+  
 
   const fetchTickets = useCallback(async (username) => {
     setLoading(true);
