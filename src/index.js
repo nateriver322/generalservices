1a5b335138ab38373916e78ac23b5a46bf0acacc
ijggarz <ijggarz01@gmail.com>
2024-11-23 10:05:03 +0800

    test login page zoom issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';  // Assuming you have an App component that will handle routing
