3931ba0a02c3f4e46f4920508ef4eaa2afedbdfd
ethan <166780996+ethannjan@users.noreply.github.com>
2024-11-26 16:23:17 +0800

    Revert "FIX LAPTOP UI"
    This reverts commit a761213281f7722417eaa0a865ce074eaf8da054.

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,7 +8,7 @@ import { useMsal } from "@azure/msal-react";
 import LoginResponsiveAppBar from './LoginResponsiveAppBar';
 import { FaMicrosoft } from 'react-icons/fa';
 import ConstructionIcon from '@mui/icons-material/Construction';
-
+import '../css/login.css';
 
 
 
@@ -86,199 +86,208 @@ const Login = React.memo(() => {
     return (
         <div
             style={{
-                position: 'relative',
+                position: 'relative', 
                 height: '100vh',
                 width: '100vw',
             }}
         >
+           
+              
             <LoginResponsiveAppBar />
             <Box
                 sx={{
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
-                    height: 'calc(100vh - 64px)', // Adjusted to leave space for the AppBar
-                    padding: 2,
-                    bgcolor: '#f5f5f5',
+                    marginBottom: '20px',
+                    marginTop: '30px'
+                }}
+            >   
+            </Box>
+            <Box
+                component="form"
+                onSubmit={handleLoginSubmit}
+                sx={{
+                    maxWidth: '400px',
+                    width: '100%',
+                    bgcolor: 'white',
+                    p: 4,
+                    borderRadius: 2,
+                    boxShadow: 3,
+                    margin: '0 auto',
+                    mt: 4
                 }}
             >
-                <Box
-                    component="form"
-                    onSubmit={handleLoginSubmit}
-                    sx={{
-                        maxWidth: { xs: '90%', sm: '400px' }, // Dynamic width for smaller devices
-                        width: '100%',
-                        bgcolor: 'white',
-                        p: 4,
-                        borderRadius: 2,
-                        boxShadow: 3,
-                    }}
+                <Box 
+                    display="flex" 
+                    flexDirection="row" 
+                    alignItems="center" 
+                    justifyContent="center" 
+                    textAlign="center" 
+                    mb={4}
                 >
-                    {/* Logo Section */}
-                    <Box
-                        display="flex"
-                        flexDirection="row"
-                        alignItems="center"
-                        justifyContent="center"
-                        textAlign="center"
-                        mb={4}
-                    >
-                        <ConstructionIcon sx={{ fontSize: 60, mb: 2, mr: 2 }} />
-                        <Typography variant="h4" component="h2" gutterBottom>
-                            JobTrack
-                        </Typography>
-                    </Box>
-    
-                    {/* Login Header */}
-                    <Typography variant="h5" component="h3" gutterBottom>
-                        Log In
+                    <ConstructionIcon sx={{ fontSize: 60, mb: 2, mr: 2 }} />
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        JobTrack
                     </Typography>
-    
-                    {error && <Typography color="error" gutterBottom>{error}</Typography>}
-    
-                    {/* Email Input */}
-                    <TextField
-                        label="Email"
-                        type="email"
-                        name="email"
-                        fullWidth
-                        required
-                        margin="normal"
-                        onChange={handleInputChange}
-                        value={credentials.email}
-                        inputProps={{ minLength: 3, maxLength: 80 }}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': { borderColor: 'black' },
-                                '&:hover fieldset': { borderColor: '#922B21' },
-                                '&.Mui-focused fieldset': { borderColor: '#800000' },
-                            },
-                            '& .MuiInputLabel-root': { color: 'black' },
-                            '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
-                        }}
-                    />
-    
-                    {/* Password Input */}
-                    <TextField
-                        label="Password"
-                        type={showPassword ? 'text' : 'password'}
-                        name="password"
-                        fullWidth
-                        required
-                        margin="normal"
-                        onChange={handleInputChange}
-                        value={credentials.password}
-                        inputProps={{ minLength: 8, maxLength: 80 }}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        onClick={togglePasswordVisibility}
-                                        edge="end"
-                                    >
-                                        {showPassword ? (
-                                            <AiOutlineEye />
-                                        ) : (
-                                            <AiOutlineEyeInvisible />
-                                        )}
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': { borderColor: 'black' },
-                                '&:hover fieldset': { borderColor: '#922B21' },
-                                '&.Mui-focused fieldset': { borderColor: '#800000' },
-                            },
-                            '& .MuiInputLabel-root': { color: 'black' },
-                            '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
-                        }}
-                    />
-    
-                    {/* Action Buttons */}
-                    <Box sx={{ mt: 3 }}>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            sx={{
-                                bgcolor: '#800000',
-                                '&:hover': { bgcolor: '#A00000' },
-                                color: 'white',
-                                mb: 2,
-                            }}
-                            fullWidth
-                        >
-                            Log In
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="text"
-                            fullWidth
-                            onClick={handleForgotPasswordClick}
-                            sx={{ color: '#800000', mb: 1 }}
-                        >
-                            Forgot Password?
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="outlined"
-                            fullWidth
-                            onClick={handleSignUpClick}
-                            sx={{
-                                borderColor: '#800000',
-                                color: '#800000',
-                                mb: 2,
-                            }}
-                        >
-                            Sign Up
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="outlined"
-                            fullWidth
-                            onClick={handleMicrosoftLogin}
-                            startIcon={<FaMicrosoft size={20} />}
-                            sx={{
-                                borderColor: '#800000',
-                                color: '#800000',
-                                mb: 2,
-                            }}
-                        >
-                            Login with Microsoft
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="outlined"
-                            fullWidth
-                            onClick={handleLogInAsPersonnelClick}
-                            sx={{
-                                borderColor: '#800000',
-                                color: '#800000',
-                            }}
-                        >
-                            Log In as Personnel
-                        </Button>
-                    </Box>
+                            
                 </Box>
+                <Typography variant="h5" component="h3" gutterBottom>
+                    Log In
+                </Typography>
+
+                {error && <Typography color="error" gutterBottom>{error}</Typography>}
+
+                <TextField
+                    label="Email"
+                    type="email"
+                    name="email"
+                    fullWidth
+                    required
+                    margin="normal"
+                    onChange={handleInputChange}
+                    value={credentials.email}
+                    inputProps={{ minLength: 3, maxLength: 80 }}
+                    sx={{
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                                borderColor: 'black',
+                            },
+                            '&:hover fieldset': {
+                                borderColor: '#922B21',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: '#800000',
+                            },
+                        },
+                        '& .MuiInputLabel-root': {
+                            color: 'black',
+                        },
+                        '& .MuiInputLabel-root.Mui-focused': {
+                            color: 'black',
+                        },
+                    }}
+                />
+
+                <TextField
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    name="password"
+                    fullWidth
+                    required
+                    margin="normal"
+                    onChange={handleInputChange}
+                    value={credentials.password}
+                    inputProps={{ minLength: 8, maxLength: 80 }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={togglePasswordVisibility} edge="end">
+                                    {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                    sx={{
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                                borderColor: 'black',
+                            },
+                            '&:hover fieldset': {
+                                borderColor: '#922B21',
+                            },
+                            '&.Mui-focused fieldset': {
+                                borderColor: '#800000',
+                            },
+                        },
+                        '& .MuiInputLabel-root': {
+                            color: 'black',
+                        },
+                        '& .MuiInputLabel-root.Mui-focused': {
+                            color: 'black',
+                        },
+                    }}
+                />
+
+<Box sx={{ mt: 2 }}>
+    <Button
+        type="submit"
+        variant="contained"
+        sx={{
+            bgcolor: '#800000',
+            '&:hover': {
+                bgcolor: '#A00000',
+            },
+            color: 'white',
+            mb: 2
+        }}
+        fullWidth
+    >
+        Log In
+    </Button>
+
+    {/* Forgot Password Button */}
+    <Button
+        type="button"
+        variant="text"
+        fullWidth
+        onClick={handleForgotPasswordClick}
+        sx={{ color: '#800000', mb: 1 }}  // Reduced margin-bottom
+    >
+        Forgot Password?
+    </Button>
+
+    <Button
+        type="button"
+        variant="outlined"
+        fullWidth
+        onClick={handleSignUpClick}
+        sx={{
+            borderColor: '#800000',
+            color: '#800000',
+            mb: 2,  // This margin is kept for spacing between the buttons
+        }}
+    >
+        Sign Up
+    </Button>
+
+    <Button
+        type="button"
+        variant="outlined"
+        fullWidth
+        onClick={handleMicrosoftLogin}
+        startIcon={<FaMicrosoft size={20} style={{ marginRight: 10 }} />}
+        sx={{
+            borderColor: '#800000',
+            color: '#800000',
+            mb: 2,
+        }}
+    >
+        Login with Microsoft
+    </Button>
+    
+    <Button
+        type="button"
+        variant="outlined"
+        fullWidth
+        onClick={handleLogInAsPersonnelClick}
+        sx={{ 
+            borderColor: '#800000',
+            color: '#800000' }} 
+    >
+        Log In as Personnel
+    </Button>
+</Box>
             </Box>
-            {/* Backdrop */}
             <Backdrop
                 sx={{
                     color: '#fff',
                     zIndex: (theme) => theme.zIndex.drawer + 1,
-                    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                    backgroundColor: 'rgba(0, 0, 0, 0.7)'
                 }}
                 open={loading}
             >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        gap: 2,
-                    }}
-                >
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
                     <CircularProgress color="info" />
                     <Typography variant="h6" component="div">
                         Logging in...
@@ -287,7 +296,6 @@ const Login = React.memo(() => {
             </Backdrop>
         </div>
     );
-    
 });
 
 export default Login;
