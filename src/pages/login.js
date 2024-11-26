25433395b74cf9cbe29ee4a3a95c56111931fb19
ethan <166780996+ethannjan@users.noreply.github.com>
2024-11-26 16:56:20 +0800

    Revert "tryyyyy"
    This reverts commit fce147ea56cf518e3340f1d2c11b3aa5b1d7618d.

--- src/pages/login.js
+++ src/pages/login.js
@@ -81,31 +81,13 @@ const Login = React.memo(() => {
         setShowPassword(!showPassword);
     };
 
-    const theme = createTheme({
-        typography: {
-          fontSize: 16, // Adjust the base font size as needed
-        },
-        breakpoints: {
-          values: {
-            xs: 0,
-            sm: 600,
-            md: 900,
-            lg: 1200,
-            xl: 1536,
-          },
-        },
-      });
     return (
         <div
-        style={{
-            position: 'relative',
-            height: '100vh',
-            width: '100vw',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
+            style={{
+                position: 'relative', 
+                height: '100vh',
+                width: '100vw',
+            }}
         >
            
               
@@ -121,18 +103,18 @@ const Login = React.memo(() => {
             >   
             </Box>
             <Box
-      component="form"
-      onSubmit={handleLoginSubmit}
-      sx={{
-        maxWidth: { xs: '90%', sm: '400px' },
-        width: '100%',
-        bgcolor: 'white',
-        p: 4,
-        borderRadius: 2,
-        boxShadow: 3,
-        margin: '0 auto',
-        mt: 4,
-      }}
+                component="form"
+                onSubmit={handleLoginSubmit}
+                sx={{
+                    maxWidth: '400px',
+                    width: '100%',
+                    bgcolor: 'white',
+                    p: 4,
+                    borderRadius: 2,
+                    boxShadow: 3,
+                    margin: '0 auto',
+                    mt: 4
+                }}
             >
                 <Box 
                     display="flex" 
@@ -182,7 +164,6 @@ const Login = React.memo(() => {
                         '& .MuiInputLabel-root.Mui-focused': {
                             color: 'black',
                         },
-                        width: { xs: '100%', sm: 'auto' }, // Adjust width for smaller screens
                     }}
                 />
 
@@ -223,7 +204,6 @@ const Login = React.memo(() => {
                         '& .MuiInputLabel-root.Mui-focused': {
                             color: 'black',
                         },
-                        width: { xs: '100%', sm: 'auto' }, // Adjust width for smaller screens
                     }}
                 />
 
