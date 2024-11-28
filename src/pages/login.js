8216ca240fc1412a085300e13311841cc775452d
ethan <166780996+ethannjan@users.noreply.github.com>
2024-11-28 19:53:00 +0800

    Revert "Update login.js"
    This reverts commit 3d0c7be39daa2115261447ab4c4b9e5656dfdcd0.

--- src/pages/login.js
+++ src/pages/login.js
@@ -84,11 +84,9 @@ const Login = React.memo(() => {
     return (
         <div
             style={{
-                position: 'relative',
-                minHeight: '100vh',
-                width: '100%',
-                margin: '0 auto',
-                overflow: 'hidden',
+                position: 'relative', 
+                height: '100vh',
+                width: '100vw',
             }}
         >
            
@@ -108,14 +106,14 @@ const Login = React.memo(() => {
                 component="form"
                 onSubmit={handleLoginSubmit}
                 sx={{
-                    maxWidth: { xs: '95%', sm: '400px' },
+                    maxWidth: '400px',
                     width: '100%',
                     bgcolor: 'white',
-                    p: { xs: 2, sm: 4 },
+                    p: 4,
                     borderRadius: 2,
                     boxShadow: 3,
                     margin: '0 auto',
-                    mt: 4,
+                    mt: 4
                 }}
             >
                 <Box 
